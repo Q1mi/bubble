259370f5f64035fd90ee2ea1cb7faf8646fc8c26
李文周 <liwenzhou7@gmail.com>
2019-11-05 23:08:05 +0800

    Delete move.sh

--- move.sh
+++
@@ -1,3 +0,0 @@
-cp -Rf /Users/office/workspace/vue/dsb/dist/static/* /Users/office/workspace/go/liwenzhou.com/bubble/static/
-cp -Rf /Users/office/workspace/vue/dsb/dist/index.html /Users/office/workspace/go/liwenzhou.com/bubble/templates/
-cp -Rf /Users/office/workspace/vue/dsb/dist/favicon.ico /Users/office/workspace/go/liwenzhou.com/bubble/templates/
